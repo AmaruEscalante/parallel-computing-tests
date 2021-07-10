883e5cd9be599fb92f1443edcb5f33edc1c70c82
amaruescalante <amaruescalante@gmail.com>
2021-07-09 19:34:02 -0500

    updated number of friends and simulation

--- p6-hybrid/01-guess-my-number.cpp
+++ p6-hybrid/01-guess-my-number.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <mpi.h>
 
-#define FRIENDS 5
+#define FRIENDS 3
+#define TESTS 10000.0
+
+#define TRIES 1000.0
 
 bool is_equal(const int array[], int n)
 {
@@ -17,7 +20,8 @@ bool is_equal(const int array[], int n)
 
 int main(int argc, char *argv[])
 {
-    int rank, size, x, res[FRIENDS], success_counter, failure_counter;
+    int rank, size, x, res[FRIENDS], success_counter, failure_counter, guess, success_rate;
+    double rate;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -29,35 +33,49 @@ int main(int argc, char *argv[])
     if (rank == 0)
     {
         printf("incia calculo random \n");
+        success_rate = 0;
         success_counter = 0;
-        failure_counter = 0; // counter de intentos fallidos inicializado en cero
+        failure_counter = 0;       // counter de intentos fallidos inicializado en cero
+        guess = (rand() % 99) + 1; // Numero de Gabriel
     }
-    for (int i = 0; i < 1000; i++)
+
+    for (int t = 0; t < TESTS; t++)
     {
-
-        x = (rand() % 99) + 1;
-        //printf("Desde %i de %i x: %i \n", rank, size, x);
-
-        MPI_Gather(&x, 1, MPI_INT, &res[0], 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
-        //printf("holaa procesos");
-        if (rank == 0)
+        for (int i = 0; i < TRIES; i++) // Intentos que tienen los 5 amigos
         {
-            if (is_equal(res, FRIENDS))
-                success_counter++;
-            else
-                failure_counter++;
-            // if (is_equal(res, FRIENDS))
-            //     printf("Son iguales\n");
-            // else
-            //     printf("No son iguales\n");
+
+            x = (rand() % 99) + 1;
+            //printf("Desde %i de %i x: %i \n", rank, size, x);
+
+            MPI_Gather(&x, 1, MPI_INT, &res[0], 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Barrier(MPI_COMM_WORLD);
+            //printf("holaa procesos");
+            if (rank == 0)
+            {
+                if (is_equal(res, FRIENDS))
+                {
+                    success_counter++;
+                    if (res[0] == guess)
+                    {
+                        success_rate++;
+                    }
+                }
+                else
+                {
+                    failure_counter++;
+                }
+            }
+            //    }
         }
-        //    }
     }
+
     if (rank == 0)
     {
+        rate = ((double)success_rate / TESTS) * 100;
         printf("Cantidad de fallos: %i\n", failure_counter);
         printf("Cantidad de aciertos: %i\n", success_counter);
+        printf("Numero de aciertos: %i\n", success_rate);
+        printf("Ratio de exito: %5.5f % \n", rate);
     }
     MPI_Finalize();
 }
