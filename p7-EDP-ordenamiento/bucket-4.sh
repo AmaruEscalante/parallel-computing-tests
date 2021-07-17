fdebd8defb4e37d38594fe2a2ad30a8ba93fb27d
JamesAtachagua <james.atachagua@utec.edu.pe>
2021-07-17 01:14:28 -0300

    scripts for khipu

---
+++ p7-EDP-ordenamiento/bucket-4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J bucket2 # nombre del job
+#SBATCH -p investigacion # nombre de la particion 
+#SBATCH -N 4 # numero de nodos
+#SBATCH --tasks-per-node=3 # numero de tasks por nodo
+
+module load openmpi/2.1.6 # carga el modulo de openmpi version 2.1.6
+srun bucketsort # bucketsort algorithm
+module unload openmpi/2.1.6 
